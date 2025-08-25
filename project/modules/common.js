0c59a819b9f71305369c184ace7c96a09195768c
restarea92 <restarea@me.com>
2025-08-25 19:59:53 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -28,8 +28,8 @@ const common = {
         this.refreshDimensions();
         this.debugScrolling();
 
-        window.addEventListener('scroll', () => this.updateScrollingState());
-        window.addEventListener('resize', () => this.refreshDimensions());ㅇ
+        window.addEventListener('scroll', (event) => this.updateScrollingState());
+        window.addEventListener('resize', (event) => this.refreshDimensions());
     },
     
     refreshDimensions() {
