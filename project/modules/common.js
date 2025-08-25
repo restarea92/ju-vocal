14548f7c873f7297009b53349c788f58314201f0
restarea92 <restarea@me.com>
2025-08-25 20:33:31 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -56,7 +56,6 @@ const common = {
             clearTimeout(resizeTimeout);
             this.updateResizeState(true);
             resizeTimeout = setTimeout(() => {
-                console.log('Resize event ended');
                 this.updateResizeState(false);
             }, 100);
         });
