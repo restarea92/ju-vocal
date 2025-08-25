4bd72bd9a4c10d0858be593708772df8e4e9d43b
restarea92 <restarea@me.com>
2025-08-25 20:13:04 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -29,7 +29,7 @@ const common = {
         this.debugScrolling();
 
         window.addEventListener('scroll', (event) => {
-            console.log(event.target)
+            console.log(event)
             this.updateScrollingState();
         });
         
