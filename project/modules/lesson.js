b74fb5b36a6871afb27a265524c1d5d10017fc96
restarea92 <restarea@me.com>
2025-08-25 22:27:57 +0900

    refeash cache

--- project/modules/lesson.js
+++ project/modules/lesson.js
@@ -107,16 +107,7 @@ const lessonApp = {
                     start:  `top+=${i * gridCardRatio}% top+=40%`,
                     end: `top+=${(i) * gridCardRatio }% 10%`,
                 });
-                const timeline1NoScrub = this.createTimeline({
-                    start:  `top+=${i * gridCardRatio}% top+=40%`,
-                    end: `top+=${(i) * gridCardRatio }% 10%`,
-                    scrub: false,
-                });
 
-                timeline1NoScrub.to(card, {
-                    border: "4px solid red",
-                });
-                
                 timeline1.to(card, {
                     ease: "power1.inOut",
                     yPercent: 0,
