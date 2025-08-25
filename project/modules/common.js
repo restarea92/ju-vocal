0a157e992b6f8b95c1563673eff4d3fb9e4066e9
restarea92 <restarea@me.com>
2025-08-25 21:57:25 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -30,7 +30,6 @@ const common = {
     updateViewportHeight() {
         clearTimeout(this.state.events.resizeTimeout);
         const newLvh = this.toPx('1lvh');
-        document.querySelector('#lvh span').textContent = `lvh: ${newLvh}`;
 
         if (this.state.events.onScroll || this.state.events.onTouchScroll) {
             if (this.state.lvh < newLvh) {
