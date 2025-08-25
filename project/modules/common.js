eab89c4829f967adcd18fed1e1c6876ef726f8a5
restarea92 <restarea@me.com>
2025-08-25 21:45:21 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -132,6 +132,7 @@ const common = {
         });
 
         window.addEventListener('touchstart', () => {
+            clearTimeout(touchScrollTimeout);
             this.state.events.onTouch = true;
         });
 
