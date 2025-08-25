b99b01dfb5afa6146ebbfeb1af425bd44b5122f2
restarea92 <restarea@me.com>
2025-08-25 20:46:12 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -120,6 +120,7 @@ const common = {
 
         window.addEventListener('resize', () => {
             if (this.state.events.onScroll) {
+                this.state.events.needResize = true;
             } else {
             clearTimeout(resizeTimeout);
                 this.updateResizeState(true);
