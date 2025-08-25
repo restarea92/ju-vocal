0e5c781512c93e6c265903a5b47a340d9cf23310
restarea92 <restarea@me.com>
2025-08-25 18:40:41 +0900

    refeash cache

--- project/modules/common.js
+++ project/modules/common.js
@@ -31,7 +31,7 @@ const common = {
     
     updateHeaderHeight() {
         const newHeight = this.elements.header?.getBoundingClientRect().height || 0;
-        console.log(this.state.elements.header);
+        console.log(this.elements.header);
         if (this.state.headerHeight !== newHeight) {
             this.elements.root.style.setProperty('--header-height', `${newHeight}px`);
             this.state.headerHeight = newHeight;
